6e9f3cb89d8ca438171e7f1e187d6134aec6868b
Riya-Panhotra <77095499+Riya-Panhotra@users.noreply.github.com>
2021-04-06 11:59:41 +0530

    Final Work

---
+++ aopconcept/src/main/java/com/ashish/aop/service/PaymentServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ashish.aop.service;
+
+public class PaymentServiceImpl  implements PaymentService
+{
+
+	public void makePayment() {
+	
+		
+	// insert code here without changing your code	
+	System.out.println("amount debited");
+	
+	
+	System.out.println("amount credited");
+	// insert code here without changing your code	
+		
+	}
+
+}
